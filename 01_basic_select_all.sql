98ef2d9e50fad6d6d2fb2340771fc8e0e02f328d
lithikajmalothu-16 <lithikajmalothu@gmail.com>
2026-06-09 08:55:19 -0400

    Add PostgreSQL exercises and queries

--- 01_basic_select_all.sql
+++ 01_basic_select_all.sql
@@ -1,8 +1,8 @@
--- Exercise: Retrieve everything from a table
+-- Exercise 1: Retrieve everything from a table
 -- Source: pgexercises.com
 
 SELECT * FROM cd.facilities;
 
----Exercise: Retrieve all the facilities names and member costs
+---Exercise 2: Retrieve all the facilities names and member costs
 SELECT name, membercost FROM cd.facilities;
 
--- 02_filtering_rows.sql
+++ 02_filtering_rows.sql
@@ -1,7 +1,7 @@
----Exercise: How can you produce a list of facilities that charge a fee to members?
+---Exercise 3: How can you produce a list of facilities that charge a fee to members?
 select * from cd.facilities where membercost>0;
 
---Exrcise: How can you produce a list of facilities that charge a fee to members, 
+--Exercise 4: How can you produce a list of facilities that charge a fee to members, 
 --and that fee is less than 1/50th of the monthly maintenance cost? 
 ---Return the facid, facility name, member cost, and monthly maintenance of the facilities in question.
 
@@ -10,3 +10,7 @@ select facid, name, membercost, monthlymaintenance
 	where 
 		membercost > 0 and 
 		(membercost < monthlymaintenance/50.0);          
+
+---Exercise 5:How can you produce a list of all facilities with the word 'Tennis' in their name?
+---use single quotes to match the terms, and % symbol to return the word (even if extra terms are surrounding it).
+select *from cd.facilities where name like '%Tennis%'; 
