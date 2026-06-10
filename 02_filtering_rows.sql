0ea2207ff1e3d57e0fa84f37a9b04aed890ae0d1
lithikajmalothu-16 <lithikajmalothu@gmail.com>
2026-06-10 14:14:48 -0400

    Add PostgreSQL exercises and queries

--- 02_filtering_rows.sql
+++ 02_filtering_rows.sql
@@ -21,3 +21,23 @@ select * from cd.facilities where facid = 1 or facid= 5;
 --without OR:
 select * from cd.facilities 
 where facid in (1,5);
+
+
+---Exercise 7: How can you produce a list of facilities, with each labelled as 'cheap' 
+---or 'expensive' depending on if their 
+---monthly maintenance cost is more than $100? Return the name and monthly maintenance 
+---of the facilities in question.
+
+select name, 
+	case when (monthlymaintenance > 100) then
+		'expensive'
+	else
+		'cheap'
+	end as cost
+	from cd.facilities;          
+
+--The second new concept is the CASE statement itself. CASE is effectively like if/switch
+--- statements in other languages, with a form as shown in the query. 
+--To add a 'middling' option, we would simply insert another when...then section.
+-- the AS operator. This is simply used to label columns or expressions, to make them display 
+--more nicely or to make them easier to reference when used as part of a subquery.
