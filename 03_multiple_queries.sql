888c86050c10eb11f3576b6264b7676390d9b20d
lithikajmalothu-16 <lithikajmalothu@gmail.com>
2026-06-11 12:52:23 -0400

    Add PostgreSQL exercises and queries

--- 03_multiple_queries.sql
+++ 03_multiple_queries.sql
@@ -1,3 +1,9 @@
 --Excersice 10:a combined list of all surnames and all facility names. Yes, this is a contrived example 
 select surname from cd.members union select name from cd.facilities;
 -- the union in sql acts like 'and' in python 
+
+
+---Excersice 11: You'd like to get the signup date of your last member. How can you retrieve this information?
+select max(joindate) as latest from cd.members;
+-- max (column_name) provides the most recent information.
+-- "as" lets us name the output column.
