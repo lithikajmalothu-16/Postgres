577457601de3c893303ec3041f7e90dc9b05d523
lithikajmalothu-16 <lithikajmalothu@gmail.com>
2026-06-12 13:59:38 -0400

    Add PostgreSQL exercises and queries

--- 04_joins_and_subqueries.sql
+++ 04_joins_and_subqueries.sql
@@ -7,4 +7,16 @@ select bks.starttime from cd.bookings bks
 		mems.firstname='David' 
 		and mems.surname='Farrell';          
 
-        
+--How can you produce a list of the start times for bookings for tennis courts, 
+--for the date '2012-09-21'? Return a list of start time and facility name pairings, ordered by the time.
+
+select bks.starttime as start, facs.name as name
+	from 
+		cd.facilities facs
+		inner join cd.bookings bks
+			on facs.facid = bks.facid
+	where 
+		facs.name in ('Tennis Court 2','Tennis Court 1') and
+		bks.starttime >= '2012-09-21' and
+		bks.starttime < '2012-09-22'
+order by bks.starttime;  
