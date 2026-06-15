72edf66ab86fa8f3d3e7996e7a8bcd6d22b9b0d8
lithikajmalothu-16 <lithikajmalothu@gmail.com>
2026-06-15 07:50:27 -0400

    Joins and Sub-Queries

--- 04_joins_and_subqueries.sql
+++ 04_joins_and_subqueries.sql
@@ -11,16 +11,17 @@ WHERE mems.firstname = 'David' and mems.surname = 'Farrell';
 --How can you produce a list of the start times for bookings for tennis courts, 
 --for the date '2012-09-21'? Return a list of start time and facility name pairings, ordered by the time.
 
-select bks.starttime as start, facs.name as name
-	from 
-		cd.facilities facs
-		inner join cd.bookings bks
-			on facs.facid = bks.facid
-	where 
-		facs.name in ('Tennis Court 2','Tennis Court 1') and
-		bks.starttime >= '2012-09-21' and
-		bks.starttime < '2012-09-22'
-order by bks.starttime;  
+
+---INNER JOIN, SQL treats the two separate tables as one big combined table for the duration of that query.
+-- Because they are joined, you can freely select columns from either table in your SELECT clause, 
+--regardless of which table you listed first in the FROM clause
+
+SELECT bks.starttime as start, facs.name as name  -- new column names.
+FROM cd.bookings bks -- can be cd.facilities facs
+INNER JOIN cd.facilities facs ON facs.facid = bks.facid
+WHERE facs.name in ('Tennis Court 1','Tennis Court 2')
+AND bks.starttime >= '2012-09-21' AND bks.starttime < '2012-09-22'
+ORDER BY bks.starttime;
 
 
 
