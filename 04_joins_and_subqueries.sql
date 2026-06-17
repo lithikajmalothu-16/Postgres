1177699650b06d8db04c509f85f4ad85fa0b1890
lithikajmalothu-16 <lithikajmalothu@gmail.com>
2026-06-17 13:57:43 -0400

    Joins and Sub-Queries

--- 04_joins_and_subqueries.sql
+++ 04_joins_and_subqueries.sql
@@ -28,7 +28,7 @@ ORDER BY bks.starttime;
 ---How can you output a list of all members who have recommended another member? 
 ---Ensure that there are no duplicates in the list, and that results are ordered by (surname, firstname).
 
-SELECT distinct recs.firstname as firstname, recs.surname as surname
+SELECT distinct recs.firstname as firstname, recs.surname as surname -- we use the new alias named table as output belongs to the new table.
 FROM cd.members mems 
 INNER JOIN cd.members recs ON  -- to create another copy of the same table-> use a different alias.
 recs.memid = mems.recommendedby --(this was the original name of the row)
@@ -44,3 +44,46 @@ select mems.firstname as memfname, mems.surname as memsname, recs.firstname as r
 		left outer join cd.members recs
 			on recs.memid = mems.recommendedby
 order by memsname, memfname; 
+
+
+---How can you produce a list of all members who have used a tennis court? 
+--Include in your output the name of the court, and the name of the member formatted as a single column. 
+--Ensure no duplicate data, and order by the member name followed by the facility name.
+
+select distinct mems.firstname || ' ' || mems.surname as member, facs.name as facility
+	from 
+		cd.members mems
+		inner join cd.bookings bks
+			on mems.memid = bks.memid
+		inner join cd.facilities facs
+			on bks.facid = facs.facid
+	where
+		facs.name in ('Tennis Court 2','Tennis Court 1')
+order by member, facility 
+
+--How can you produce a list of bookings on the day of 2012-09-14 which will cost the member (or guest) more than $30? 
+--Remember that guests have different costs to members (the listed costs are per half-hour 'slot'),and the guest user is always ID 0. 
+--Include in your output the name of the facility, the name of the member formatted as a single column, and the cost. 
+--Order by descending cost, and do not use any subqueries.
+
+select mems.firstname || ' ' || mems.surname as member, 
+	facs.name as facility, 
+	case 
+		when mems.memid = 0 then
+			bks.slots*facs.guestcost
+		else
+			bks.slots*facs.membercost
+	end as cost
+        from
+                cd.members mems                
+                inner join cd.bookings bks
+                        on mems.memid = bks.memid
+                inner join cd.facilities facs
+                        on bks.facid = facs.facid
+        where
+		bks.starttime >= '2012-09-14' and 
+		bks.starttime < '2012-09-15' and (
+			(mems.memid = 0 and bks.slots*facs.guestcost > 30) or
+			(mems.memid != 0 and bks.slots*facs.membercost > 30)
+		)
+order by cost desc;  
